e1014df5cf5d0398ee20b42c83102a6d5f000e58
Vv-kK <veronicakylie1@gmail.com>
2024-04-16 21:33:16 +0700

    [RED] Make tests for PromoCodeController

---
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroombeadmin/controller/PromoCodeControllerTest.java
@@ -0,0 +1,58 @@
+package id.ac.ui.cs.advprog.hoomgroombeadmin.controller;
+
+import id.ac.ui.cs.advprog.hoomgroombeadmin.controller.PromoCodeController;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = PromoCodeController.class)
+public class PromoCodeControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @InjectMocks
+    private PromoCodeController controller;
+
+    @Test
+    public void createPromoCodeTest() throws Exception {
+        mvc.perform(post("/admin/promo-code/create"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updatePromoCodePostTest() throws Exception {
+        mvc.perform(post("/admin/promo-code/update/save"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updatePromoCodePageTest() throws Exception {
+        UUID promoCodeId = new UUID(32, 10);
+        mvc.perform(get("/admin/promo-code/update/" + promoCodeId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deletePromoCodeTest() throws Exception {
+        UUID promoCodeId = new UUID(32, 10);
+        mvc.perform(post("/admin/promo-code/delete/" + promoCodeId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void listPromoCodeTest() throws Exception {
+        mvc.perform(get("/admin/promo-code/manage"))
+                .andExpect(status().isOk());
+    }
+}
