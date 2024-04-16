c6e5a237634db195c735c9aed67e33efd3aed177
Vv-kK <veronicakylie1@gmail.com>
2024-04-16 14:17:46 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroombeadmin/HoomgroomBeAdminApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.hoomgroombeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoomgroomBeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoomgroomBeAdminApplication.class, args);
+	}
+
+}
