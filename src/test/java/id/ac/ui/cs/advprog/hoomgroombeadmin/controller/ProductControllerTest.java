bc6ed0254eb4ed630dce41856859aded3a9e3a6d
Vv-kK <veronicakylie1@gmail.com>
2024-04-16 19:46:17 +0700

    [RED] Making tests for non functional product controller

---
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroombeadmin/controller/ProductControllerTest.java
@@ -0,0 +1,58 @@
+package id.ac.ui.cs.advprog.hoomgroombeadmin.controller;
+
+import id.ac.ui.cs.advprog.hoomgroombeadmin.controller.ProductController;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = ProductController.class)
+public class ProductControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @InjectMocks
+    private ProductController controller;
+
+    @Test
+    public void createProductTest() throws Exception {
+        mvc.perform(get("/admin/product/create"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateProductPostTest() throws Exception {
+        mvc.perform(get("/admin/product/update/save"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateProductPageTest() throws Exception {
+        UUID productId = new UUID(32, 10);
+        mvc.perform(get("/admin/product/update/" + productId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteProductTest() throws Exception {
+        UUID productId = new UUID(32, 10);
+        mvc.perform(get("/admin/product/delete/" + productId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void listProductTest() throws Exception {
+        mvc.perform(get("/product/list/"))
+                .andExpect(status().isOk());
+    }
+}
