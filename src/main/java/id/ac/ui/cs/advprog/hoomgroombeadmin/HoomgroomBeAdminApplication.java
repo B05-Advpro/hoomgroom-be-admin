06ee716d2e7dac48ea0c5a5a5bb263fbc563f140
Vv-kK <veronicakylie1@gmail.com>
2024-04-16 20:33:32 +0700

    [GREEN] Make non functional ProductController

--- src/main/java/id/ac/ui/cs/advprog/hoomgroombeadmin/HoomgroomBeAdminApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroombeadmin/HoomgroomBeAdminApplication.java
@@ -2,8 +2,9 @@ package id.ac.ui.cs.advprog.hoomgroombeadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HoomgroomBeAdminApplication {
 
 	public static void main(String[] args) {
--- src/main/java/id/ac/ui/cs/advprog/hoomgroombeadmin/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroombeadmin/controller/ProductController.java
@@ -1,4 +1,36 @@
 package id.ac.ui.cs.advprog.hoomgroombeadmin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Controller
 public class ProductController {
+
+    @PostMapping("/admin/product/create")
+    public String createProductPost(Model model){
+        return "Hello, world!";
+    }
+
+    @PostMapping("/admin/product/update/save")
+    public String updateProductPost(Model model) {
+        return "Hello, world!";
+    }
+
+    @GetMapping("/admin/product/update/{productId}")
+    public String updateProductPage(@PathVariable UUID productId, Model model){
+        return "Hello, world!";
+    }
+
+    @PostMapping("/admin/product/delete/{productId}")
+    public String deleteProduct(@PathVariable UUID productId, Model model){
+        return "Hello, world!";
+    }
+
+    @GetMapping("/product/list")
+    public String listProduct(Model model){
+        return "Hello, world!";
+    }
 }
