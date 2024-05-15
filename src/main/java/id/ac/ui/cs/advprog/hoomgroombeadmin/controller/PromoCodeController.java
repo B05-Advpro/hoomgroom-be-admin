ff4180014e65d6de8d421568872e195cb624f1d7
Scarletra <112821721+Scarletra@users.noreply.github.com>
2024-05-15 20:04:06 +0700

    Fix controller for running app issue

--- src/main/java/id/ac/ui/cs/advprog/hoomgroombeadmin/controller/PromoCodeController.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroombeadmin/controller/PromoCodeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
@@ -13,7 +14,6 @@ import java.util.List;
 @RequestMapping("/admin/promo-code")
 public class PromoCodeController {
 
-    @Autowired
     private PromoCodeService service;
 
     @PostMapping("/create")
