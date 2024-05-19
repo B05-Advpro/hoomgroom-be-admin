a8f41a7f09027df82b4ff8f24d7dc92d2b94c49a
Vv-kK <veronicakylie1@gmail.com>
2024-05-19 22:58:19 +0700

    [REFACTOR] Somehow the annotation disappeared

--- src/main/java/id/ac/ui/cs/advprog/hoomgroombeadmin/controller/PromoCodeController.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroombeadmin/controller/PromoCodeController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/admin/promo-code")
 public class PromoCodeController {
 
+    @Autowired
     private PromoCodeService service;
 
     @PostMapping("/create")
